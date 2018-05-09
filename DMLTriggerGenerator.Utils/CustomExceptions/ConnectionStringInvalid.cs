c9d69c54191f68f1ff61a2960ef6a2bd93068d0c
Unknown <lev.madylius@gmail.com>
2018-05-09 14:07:02 +0300

    Connection string
    Connection string model created, custom connection string exception created

---
+++ DMLTriggerGenerator.Utils/CustomExceptions/ConnectionStringInvalid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DMLTriggerGenerator.Utils.CustomExceptions
+{
+    public class ConnectionStringInvalidException : Exception
+    {
+        public ConnectionStringInvalidException():base()
+        {
+
+        }
+        public ConnectionStringInvalidException(string message) : base(message)
+        {
+        }
+
+        public ConnectionStringInvalidException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ConnectionStringInvalidException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
