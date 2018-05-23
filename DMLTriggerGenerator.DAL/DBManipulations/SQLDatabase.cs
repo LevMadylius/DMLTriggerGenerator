b92269d32a7059a32dedc8197a3783aad21f897b
Unknown <lev.madylius@gmail.com>
2018-05-23 18:35:34 +0300

    DAL.
    Connection strings and SQLDatabase.

---
+++ DMLTriggerGenerator.DAL/DBManipulations/SQLDatabase.cs
@@ -0,0 +1,95 @@
+﻿using DMLTriggerGenerator.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMLTriggerGenerator.DAL.DBManipulations
+{
+    public static class SQLDatabase
+    {
+        private const int CommandTimeout = 180;
+
+        public static void CreateCommand(string query, CommandType commandType = CommandType.Text, params SqlParameter[] arrParam)
+        {
+            string connectionString = ConnectionString.GetConnectionString();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.CommandTimeout = CommandTimeout;
+                    command.CommandType = commandType;
+
+                    if (arrParam != null)
+                    {
+                        foreach (SqlParameter param in arrParam)
+                        {
+                            command.Parameters.Add(param);
+                        }
+                    }
+
+                    command.Connection.OpenAsync();
+                    command.ExecuteNonQueryAsync();
+                    command.Connection.Close();
+
+                }
+            }
+        }
+
+        internal static DataTable ExecuteQuery(string query, CommandType commandType = CommandType.Text, params SqlParameter[] arrParam)
+        {
+            DataTable dt = new DataTable();
+            string connectionString = ConnectionString.GetConnectionString();
+
+            using (var connect = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connect))
+                {
+
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandType = commandType;
+
+                    if (arrParam != null)
+                    {
+                        foreach (SqlParameter param in arrParam)
+                        {
+                            cmd.Parameters.Add(param);
+                        }
+                    }
+
+                    connect.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                    connect.Close();
+                }
+            }
+
+            return dt;
+        }
+        // unfinished
+        //    public static List<string> GetCollectionItems(string query, CommandType commandType = CommandType.Text, params SqlParameter[] arrParam)
+        //    {
+        //        List<string>searchResult = new List<string>();
+
+        //        using (DataTable resultTable = ExecuteQuery(query, CommandType.Text, arrParam))
+        //        {
+        //            resultTable.
+        //            foreach (DataRow row in resultTable.Rows)
+        //            {
+        //                row.
+        //                var entity = new T();
+        //                if (entity != null)
+        //                    searchResult.Add(entity);
+        //            }
+        //        }
+        //        return searchResult;
+        //    }
+        //}
+    }
---
+++ DMLTriggerGenerator.DAL/Model/ConnectionString.cs
@@ -0,0 +1,29 @@
+﻿using DMLTriggerGenerator.Utils.CustomExceptions;
+using System.Net;
+
+
+namespace DMLTriggerGenerator.DAL.Model
+{
+    public static class ConnectionString
+    {
+        public static IPAddress IP { get; set; }
+        public static int Port { get; set; }
+        public static string InitialCatalog { get; set; }
+        public static string UserId { get; set; }
+        public static string Password { get; set; }
+
+
+        public static string GetConnectionString()
+        {
+            if (IP == null || Port == 0 || string.IsNullOrEmpty(InitialCatalog) || string.IsNullOrEmpty(UserId) || string.IsNullOrEmpty(Password))
+            {
+                throw new ConnectionStringInvalidException("One of parameters is empty");
+            }
+
+            string connectionString = $"Data Source={IP.ToString()},{Port};Network Library=DBMSSOCN;Network Library={InitialCatalog};User ID={UserId};Password={Password}";
+
+
+            return connectionString;
+        }
+    }
+}
